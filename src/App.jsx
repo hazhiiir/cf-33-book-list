9c28104d968a2797b5118be22c4e674a5550aaa9
hazhiiir <mohammadmoein@arvancloud.com>
2026-02-27 00:09:29 +0330

    add books filter component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import BooksFilter from "./components/BooksFilter";
 function App() {
   return (
     <>
       <Header />
-      <p className="text-3xl font-bold underline ">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus
-        consequuntur, itaque delectus voluptatum expedita soluta, earum
-        similique, sapiente veniam molestias corporis quasi provident nobis
-        reiciendis veritatis doloremque? Blanditiis, eveniet odio.
-      </p>
+      <main className="max-w-7xl mx-auto px-4 py-8">
+        <BooksFilter />
+        <p className="text-3xl font-bold underline ">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Necessitatibus consequuntur, itaque delectus voluptatum expedita
+          soluta, earum similique, sapiente veniam molestias corporis quasi
+          provident nobis reiciendis veritatis doloremque? Blanditiis, eveniet
+          odio.
+        </p>
+      </main>
       <Footer />
     </>
   );
