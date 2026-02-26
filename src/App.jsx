4eff9c5634d70181ae0fcd5a14a3280a2d0c20e4
hazhiiir <mohammadmoein@arvancloud.com>
2026-02-27 02:18:59 +0330

    add search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { Document } from "flexsearch";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import BooksFilter from "./components/BooksFilter";
@@ -12,9 +13,53 @@ function App() {
     filter: "",
   });
 
+  const [index, setIndex] = useState(null);
+
+  useEffect(() => {
+    const indexBooks = async () => {
+      const newIndex = new Document({
+        document: {
+          id: "id",
+          index: [
+            "title",
+            "authors.name",
+            "summaries",
+            "subjects",
+            "bookshelves",
+          ],
+          store: true,
+        },
+        tokenize: "forward",
+        cache: 100,
+      });
+
+      for (let i = 0; i < booksRaw.length; ++i) {
+        newIndex.add(booksRaw[i]);
+      }
+
+      setIndex(newIndex);
+
+      Promise.resolve();
+    };
+    indexBooks();
+  }, []);
+
   const onFiltersChange = (value) => setFilters(value);
 
   let books = [...booksRaw];
+
+  if (filters.search && index) {
+    const searchResult = index.search(filters.search);
+    const targets = new Set();
+    for (let i = 0; i < searchResult.length; ++i) {
+      const results = searchResult[i].result;
+      for (let j = 0; j < results.length; j++) {
+        targets.add(results[j]);
+      }
+    }
+
+    books = books.filter((book) => targets.has(book.id));
+  }
 
   if (filters.filter) {
     books = books.filter((book) => book.languages.includes(filters.filter));
@@ -34,7 +79,7 @@ function App() {
     <>
       <Header />
       <main className="max-w-7xl mx-auto px-4 py-8">
-        <BooksFilter onChange={onFiltersChange} />
+        {index && <BooksFilter onChange={onFiltersChange} />}
         <BookList books={books} />
       </main>
       <Footer />
