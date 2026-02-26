6e601fe6d425937cfb2fff84023761edfa00ad9c
hazhiiir <mohammadmoein@arvancloud.com>
2026-02-27 01:34:29 +0330

    add filter and sort functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import BooksFilter from "./components/BooksFilter";
@@ -5,12 +6,36 @@ import BookList from "./components/BookList";
 import booksRaw from "./data/books";
 
 function App() {
+  const [filters, setFilters] = useState({
+    search: "",
+    sort: "",
+    filter: "",
+  });
+
+  const onFiltersChange = (value) => setFilters(value);
+
+  let books = [...booksRaw];
+
+  if (filters.filter) {
+    books = books.filter((book) => book.languages.includes(filters.filter));
+  }
+
+  if (filters.sort) {
+    if (filters.sort === "title") {
+      books = [...books].sort((b1, b2) => b1.title.localeCompare(b2.title));
+    } else if (filters.sort === "downloads") {
+      books = [...books].sort(
+        (b1, b2) => b2.download_count - b1.download_count,
+      );
+    }
+  }
+
   return (
     <>
       <Header />
       <main className="max-w-7xl mx-auto px-4 py-8">
-        <BooksFilter />
-        <BookList books={booksRaw} />
+        <BooksFilter onChange={onFiltersChange} />
+        <BookList books={books} />
       </main>
       <Footer />
     </>
