c5fbcdb99d2c71df6540612dd678741326dda8aa
hazhiiir <mohammadmoein@arvancloud.com>
2026-02-27 00:49:23 +0330

    add state to bookfilter component and Select component

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -3,7 +3,7 @@ export default function BookList({ books }) {
   return (
     <section
       id="books-grid"
-      class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mb-8"
+      className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mb-8"
     >
       {books &&
         books.length &&
--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,12 +1,17 @@
-export default function Select({ id, options, label }) {
+export default function Select({ id, options, label, value, onChange }) {
   return (
     <div>
-      <label for={id} className="block text-sm font-medium text-sepia-700 mb-2">
+      <label
+        htmlFor={id}
+        className="block text-sm font-medium text-sepia-700 mb-2"
+      >
         {label}
       </label>
       <select
         id={id}
         className="w-full px-4 py-3 border border-sepia-200 rounded-lg focus:ring-2 focus:ring-amber-500 focus:border-amber-500 transition-all bg-paper text-sepia-800 cursor-pointer"
+        value={value}
+        onChange={onChange}
       >
         {options &&
           options.length &&
