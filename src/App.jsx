233c9eb075345e875cd0b1a56aa708050e7f078d
hazhiiir <mohammadmoein@arvancloud.com>
2026-02-27 00:19:47 +0330

    add book list and book card components

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import BooksFilter from "./components/BooksFilter";
+import BookList from "./components/BookList";
+import booksRaw from "./data/books";
+
 function App() {
   return (
     <>
       <Header />
       <main className="max-w-7xl mx-auto px-4 py-8">
         <BooksFilter />
-        <p className="text-3xl font-bold underline ">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-          Necessitatibus consequuntur, itaque delectus voluptatum expedita
-          soluta, earum similique, sapiente veniam molestias corporis quasi
-          provident nobis reiciendis veritatis doloremque? Blanditiis, eveniet
-          odio.
-        </p>
+        <BookList books={booksRaw} />
       </main>
       <Footer />
     </>
