ec75e2add5a5db464bd73812333751a676761f2d
hazhiiir <mohammadmoein@arvancloud.com>
2026-02-26 23:04:59 +0330

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header";
+function App() {
+  return (
+    <>
+      <Header />
+      <p className="text-3xl font-bold underline ">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus
+        consequuntur, itaque delectus voluptatum expedita soluta, earum
+        similique, sapiente veniam molestias corporis quasi provident nobis
+        reiciendis veritatis doloremque? Blanditiis, eveniet odio.
+      </p>
+    </>
+  );
+}
+
+export default App;
